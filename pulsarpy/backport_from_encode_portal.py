df7137f00719dd39d245d1ab0d51fbcd45f98a4d
Nathaniel Watson <nathankw@encyclia.local>
2018-04-12 20:20:11 -0700

    Changed Model.find_by to use post instead of get. Added more model subclasses.

--- pulsarpy/backport_from_encode_portal.py
+++ pulsarpy/backport_from_encode_portal.py
@@ -6,14 +6,61 @@
 #nathankw@stanford.edu                                                                                 
 ###                                                                                                    
 
+import re
+
+import pulsarpy.models
+
+protocol_regx = re.compile(r'protocol', re.IGNORECASE)
+
+UPSTREAM_PROP = "upstream_identifier"
 
 def document(rec): 
     """
     Example document: https://www.encodeproject.org/documents/716003cd-3ce7-41ce-b1e3-6f203b6632a0/?format=json
     """
+    aliases = rec["aliases"]
+    if aliases:
+        upstream_identifier = aliases[0]
+    else:
+        upstream_identifier = rec["uuid"]
     payload = {}
-    payload["name"] = rec["attachment"]["download"]
-    payload["document_type"] = 
+    payload["description"] = rec["description"]
+    payload[UPSTREAM_PROP] = upstream_identifier
+    document_type = rec["document_type"]
+    # Determine whether this is a protocol document and set Document.is_protocol accordingly. 
+    protocol = False
+    if protocol_regx.search(document_type):
+        protocol = True
+    payload["is_protocol"] = protocol
+
+def treatment(rec):
+    """
+    The required properties in the ENCODE Portal are:
     
+    1. treatment_term_name, 
+    2. treatment_type. 
 
+    Example on the Portal: https://www.encodeproject.org/treatments/933a1ff2-43a2-4a54-9c87-aad228d0033e/.
+    """
+    payload = {}
+    #check if upstream exists already in Pulsar:
+    upstream = rec["aliases"][0]
+    exists = models.Treatment.find_by({UPSTREAM_PROP: upstream})
+    if exists:
+        return exists
 
+    payload[UPSTREAM_PROP] = upstream
+    payload["concentration"] = rec["amount"]
+    amount_units = rec["amount_units"]
+    amount_units_rec = models.ConcentrationUnit.find_by(params={"name": amount_units})
+    payload["concentration_unit_id"] = amount_units_rec["id"]
+    payload["duration"] = rec["duration"]
+    payload["duration_units"] = rec["duration_units"]
+    ttn = rec["treatment_term_name"]
+    ttn_rec = models.TreatmentTermName.find_by(params={"name": ttn)
+    if not ttn_id:
+        raise Exception("TreatmentTermName {} couldn't be found.".format(ttn))
+    payload["treatment_term_name_id"] = ttn_rec["id"]
+    payload["treatment_type"] = rec["treatment_type"]
+    payload[UPSTREAM_PROP] = rec["aliases"][0]
+    return models.Treatment.post(data=payload)
--- pulsarpy/models.py
+++ pulsarpy/models.py
@@ -53,7 +53,7 @@ import inflection
 #
 # 3) Create a new construct_tag
 #
-#    data = {'name': 'test_tag_ampc', 'description': "C'est bcp + qu'un simple ..."}
+#    payload = {'name': 'test_tag_ampc', 'description': "C'est bcp + qu'un simple ..."}
 #    r = requests.post(url=url, headers=HEADERS, verify=False, data=json.dumps({"construct_tag": {"name": "nom"}}))
 
 
@@ -69,9 +69,35 @@ class Model(metaclass=Meta):
     MODEL_NAME = ""  # subclasses define
 
     @classmethod
-    def record_url(cls, uid):
+    def add_model_name_to_payload(cls, payload):
         """
-        Function : Given a record identifier, returns the URL to the record.
+        Checks whether the model name in question is in the payload. If not, the entire payload
+        is set as a value of a key by the name of the model.  This method is useful when some 
+        server-side Rails API calls expect the parameters to include the parameterized model name.
+        For example, server-side endpoints that handle the updating of a biosample record or the 
+        creation of a new biosmample record will expect the payload to be of the form::
+
+            { "biosample": {
+                "name": "new biosample",
+                "donor": 3,
+                ...
+               }
+            }
+
+        Args:
+            payload: `dict`. The data to send in an HTTP request. 
+
+        Returns:
+            `dict`. 
+        """
+        if not cls.MODEL_NAME in payload:
+            payload = {cls.MODEL_NAME: payload}
+        return payload
+
+    @classmethod
+    def record_url(cls, uid):
+        """Given a record identifier, returns the URL to the record.
+
         Args     : uid - The database identifier of the record to fetch. Will be converted to a string.
         """
         uid = str(uid)
@@ -79,65 +105,77 @@ class Model(metaclass=Meta):
 
     @classmethod
     def delete(cls, uid):
-        """
-        Function : Deletes the record.
+        """Deletes the record.
+
         Args     : uid - The database identifier of the record to fetch. Will be converted to a string.
         """
         url = cls.record_url(uid)
-        res = requests.delete(url=url, headers=cls.HEADERS, verify=False)
+        res = requests.delete(url=url, headers=Model.HEADERS, verify=False)
         return res.json()
 
     @classmethod
-    def find_by(cls, params):
+    def find_by(cls, payload):
         """
-        Implements a Railsy way of looking for a record using a method by the same name.
+        Implements a Railsy way of looking for a record using a method by the same name. 
+        Only the first hit is returned, and there is not particular ordering specified in the server-side
+        API method. 
 
         Args:
-            params: `dict`. The attributes of a record to restrict the search to.
+            payload: `dict`. The attributes of a record to restrict the search to.
 
         Returns:
-            `dict`: The JSON serialization of the records found by the API call.
+            `dict`: The JSON serialization of the record, if any, found by the API call.
             `None`: If the API call didnt' return any results.
         """
         url = os.path.join(cls.URL, "find_by")
-        res = requests.get(url=url, params=params, headers=cls.HEADERS, verify=False)
+        payload = cls.add_model_name_to_payload(payload)
+        print("Searching {} for {}".format(cls.__name__, json.dumps(payload, indent=4)))
+        res = requests.post(url=url, data=json.dumps(payload), headers=Model.HEADERS, verify=False)
         #return res.json
         cls.write_response_html_to_file(res,"bob.html")
         return res
 
     @classmethod
     def get(cls, uid):
-        """
-        Function : Fetches a record by the records ID.
+        """Fetches a record by the records ID.
+
         Args     : uid - The database identifier of the record to fetch. Will be converted to a string.
         """
 
-        res = requests.get(url=url, headers=cls.HEADERS, verify=False)
+        url = cls.record_url(uid)
+        print("Getting {} record with ID {}: {}".format(cls.__name__, uid, url))
+        res = requests.get(url=url, headers=Model.HEADERS, verify=False)
         return res.json()
 
     @classmethod
-    def patch(cls, uid, data):
+    def index(cls):
+        """Fetches all records.
         """
-        Function : Patches the data to the specified record.
-        Args     : uid - The database identifier of the record to patch. Will be converted to a string.
-                   data - hash. This will be JSON-formatted prior to sending the request.
-        """
-        url = cls.record_url(uid)
-        if not cls.MODEL_NAME in data:
-            data = {cls.MODEL_NAME: data}
-        res = requests.patch(url=url, data=json.dumps(data), headers=cls.HEADERS, verify=False)
-        return res
+        res = requests.get(cls.URL, headers=Model.HEADERS, verify=False)
+        return res.json()
 
     @classmethod
-    def post(cls, data):
+    def patch(cls, uid, payload):
+        """Patches the payload to the specified record.
+
+        Args     : uid - The database identifier of the record to patch. Will be converted to a string.
+                   payload - hash. This will be JSON-formatted prior to sending the request.
         """
-        Function : Posts the data to the specified record.
+        url = cls.record_url(uid)
+        payload = cls.add_model_name_to_payload(payload)
+        res = requests.patch(url=url, data=json.dumps(payload), headers=Model.HEADERS, verify=False)
+        return res.json()
+
+    @classmethod
+    def post(cls, payload):
+        """Posts the data to the specified record.
+
         Args     : uid - The database identifier of the record to fetch. Will be converted to a string.
-                   data - hash. This will be JSON-formatted prior to sending the request.
+                   payload - hash. This will be JSON-formatted prior to sending the request.
         """
-        if not cls.MODEL_NAME in data:
-            data = {cls.MODEL_NAME: data}
-        return requests.post(url=cls.URL, data=json.dumps(data), headers=cls.HEADERS, verify=False)
+        payload = cls.add_model_name_to_payload(payload)
+        res = requests.post(url=cls.URL, data=json.dumps(payload), headers=Model.HEADERS, verify=False)
+        return res.json()
 
     @classmethod
     def write_response_html_to_file(cls,response,filename):
@@ -170,6 +208,8 @@ class Biosample(Model):
 class BiosampleTermName(Model):
     MODEL_NAME = "biosample_term_name"
 
+class ConcentrationUnit(Model):
+    MODEL_NAME = "concentration_unit"
 
 class ConstructTag(Model):
     MODEL_NAME = "construct_tag"
@@ -178,14 +218,20 @@ class ConstructTag(Model):
 class Document(Model):
     MODEL_NAME = "document"
 
+class Treatment(Model):
+    MODEL_NAME = "treatment"
+
+class TreatmentTermName(Model):
+    MODEL_NAME = "treatment_term_name"
+
 
 if __name__ == "__main__":
     # pdb.set_trace()
     b = Biosample()
     pdb.set_trace()
     res = b.get(uid=1716)
-    #res = b.patch(uid=1772,data={"name": "bobq_a"})
+    #res = b.patch(uid=1772,payload={"name": "bobq_a"})
     #res = b.delete(uid=1719)
     #c = ConstructTag()
-    #res = c.post(data={"name": "howdy there partner"})
+    #res = c.post(payload={"name": "howdy there partner"})
     pdb.set_trace()
