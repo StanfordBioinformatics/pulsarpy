99e1ba43c543f03f5320ef63613f1ab5e4683ee2
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-11-15 19:54:28 -0800

    Updated elasticsearch_utils.py to be class-based.

--- pulsarpy/elasticsearch_utils.py
+++ pulsarpy/elasticsearch_utils.py
@@ -8,9 +8,9 @@
 
 import os
 
+import pulsarpy
 from elasticsearch import Elasticsearch
 
-ES = Elasticsearch(os.environ["ES_URL"], http_auth=(os.environ["ES_USER"], os.environ["ES_PW"]))
 
 class MultipleHitsException(Exception):
     """
@@ -18,37 +18,45 @@ class MultipleHitsException(Exception):
     """
     pass
 
-def get_record_by_name(index, name):
-    """
-    Searches for a single document in the given index on the 'name' field .
-    Performs a case-insensitive search by utilizing Elasticsearch's `match_phrase` query.
+class Connection():
+    ES_URL = os.environ["ES_URL"]
+    ES_USER = os.environ["ES_USER"]
+    ES_PW = os.environ["ES_PW"]
+    ES_AUTH = (ES_USER, ES_PW)
+    ES = Elasticsearch(ES_URL, http_auth=ES_AUTH))
 
-    Args:
-        index: `str`. The name of an Elasticsearch index (i.e. biosamples).
-        name: `str`. The value of a document's name key to search for.
 
-    Returns:
-        `dict` containing the document that was indexed into Elasticsearch.
-
-    Raises:
-        `MultipleHitsException`: More than 1 hit is returned.
-    """
-    result = ES.search(
-        index=index,
-        body={
-            "query": {
-                "match_phrase": {
-                    "name": name,
+    def get_record_by_name(self, index, name):
+        """
+        Searches for a single document in the given index on the 'name' field .
+        Performs a case-insensitive search by utilizing Elasticsearch's `match_phrase` query.
+    
+        Args:
+            index: `str`. The name of an Elasticsearch index (i.e. biosamples).
+            name: `str`. The value of a document's name key to search for.
+    
+        Returns:
+            `dict` containing the document that was indexed into Elasticsearch.
+    
+        Raises:
+            `MultipleHitsException`: More than 1 hit is returned.
+        """
+        result = self.ES.search(
+            index=index,
+            body={
+                "query": {
+                    "match_phrase": {
+                        "name": name,
+                    }
                 }
             }
-        }
-    )
-    hits = result["hits"]["hits"]
-    if not hits:
-        return {}
-    elif len(hits) == 1:
-        return hits[0]["_source"]
-    else:
-        msg = "match_phrase search found multiple records matching query '{}' for index '{}'.".format(name, index)
-        raise MultipleHitsException(msg)
-
+        )
+        hits = result["hits"]["hits"]
+        if not hits:
+            return {}
+        elif len(hits) == 1:
+            return hits[0]["_source"]
+        else:
+            msg = "match_phrase search found multiple records matching query '{}' for index '{}'.".format(name, index)
+            raise MultipleHitsException(msg)
+    
